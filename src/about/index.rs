797653d4ac3b7fc4493e48abde2e88d3eb198606
Morten Jelle <Zoulember@gmail.com>
2019-03-20 00:38:11 +0100

    Adds /about

--- src/about/index.rs
+++ src/about/index.rs
@@ -4,7 +4,7 @@ fn handler(request: Request<()>) -> http::Result<Response<String>> {
 	let response = Response::builder()
 		.status(StatusCode::OK)
 		.header(header::CONTENT_TYPE, "text")
-		.body(format!("{} - /", request.method()))
+		.body(format!("{} - /about", request.method()))
 		.expect("failed to render response");
 
 	Ok(response)
