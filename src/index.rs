341892ba9d2631197bc4b3bcff4e093f45e8829b
Morten Jelle <Zoulember@gmail.com>
2019-03-20 12:04:31 +0100

    Changes responses to JSON

--- src/index.rs
+++ src/index.rs
@@ -1,10 +1,11 @@
 use http::{header, Request, Response, StatusCode};
+use serde_json::{json, Value};
 
-fn handler(request: Request<()>) -> http::Result<Response<String>> {
+fn handler(request: Request<()>) -> http::Result<Response<Value>> {
 	let response = Response::builder()
 		.status(StatusCode::OK)
-		.header(header::CONTENT_TYPE, "text")
-		.body(format!("{} - /", request.method()))
+		.header(header::CONTENT_TYPE, "text/json")
+		.body(json!({ "path": "/", "method": request.method().to_string() }))
 		.expect("failed to render response");
 
 	Ok(response)
